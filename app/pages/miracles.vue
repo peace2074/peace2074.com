311825107038cd0175a41cacee85a95cd28bbd76
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-03-06 22:32:34 +0200

    translate 1

--- app/pages/miracles.vue
+++ app/pages/miracles.vue
@@ -1,8 +1,22 @@
+
 <script lang="ts" setup>
+import type { ONE_INTERFACE } from "./quran/[...lok].vue"
 const { t } = useI18n()
+const nuxtApp = useNuxtApp()
+const Quran: ONE_INTERFACE[] = nuxtApp.payload.data.B6H5jvHlMH
+const CountOfAyat: Ref<number> = computed(() => {
+    return Quran.map(s=>s.type)
+})
+
 </script>
 <template>
     <q-page padding>
         <h1>{{ t('pages.miracles.pageTitle') }}</h1>
+        <q-cart>
+            <q-card-section>
+                {{ CountOfAyat }}
+            </q-card-section>
+            
+        </q-cart>
     </q-page>
 </template>
